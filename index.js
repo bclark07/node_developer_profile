0b78d7f3a060611e3138d1a50844d87bb7b1c120
Brittany Clark <clark.brittany.j@gmail.com>
2020-03-05 14:09:48 -0500

    updated ReadMe, one github api call working, added provided code to generate HTML

--- index.js
+++ index.js
@@ -1,19 +1,48 @@
 const inquirer = require("inquirer");
+const axios = require("axios");
 
 // const writeFileAsync = util.promisify(fs.writeFile);
 
-function promptUser() {
-  return inquirer
-    .prompt([
-      {
-        type: "input",
-        name: "color",
-        message: "What is your favorite color?"
-      }
-    ])
-    .then(function(data) {
-      console.log("Favorite color is " + data.color);
-    });
-}
+// function promptUser() {
+//   return
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "color",
+      message: "What is your favorite color?"
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "Enter your GitHub Username"
+    }
+  ])
+  .then(function(data) {
+    console.log("Favorite color is " + data.color);
+    const queryUrl = `https://api.github.com/users/${data.github}/repos?per_page=100`;
+    axios.get(queryUrl).then(function(res) {
+      const repoNames = res.data.map(function(repo) {
+        return repo.name;
+      });
 
-promptUser(); //generates the question in the console
+      const repoNamesStr = repoNames.join("\n");
+
+      console.log(`Saved ${repoNamesStr} repos`);
+    });
+    //get this from GitHUb:
+    //       - Profile image
+    // - User name
+    // - Links to the following:
+    //   - User location via Google Maps
+    //   - User GitHub profile
+    //   - User blog
+    // - User bio
+    // - Number of public repositories
+    // - Number of followers
+    // - Number of GitHub stars
+    // - Number of users following
+  });
+// }
+
+// promptUser(); //generates the question in the console

329f0f3c01c2661b2badc5cb1c5a43a40d27000e
Brittany Clark <clark.brittany.j@gmail.com>
2020-03-05 19:23:39 -0500

    associating data objects from api call to variables

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
+Create a command-line application that dynamically generates a PDF profile from a GitHub username. The user will be prompted for a favorite color, which will be used as the background color for cards.
+
+The PDF will be populated with the following:
+
+- Profile image
+- User name
+- Links to the following:
+  - User location via Google Maps
+  - User GitHub profile
+  - User blog
+- User bio
+- Number of public repositories
+- Number of followers
+- Number of GitHub stars
+- Number of users following
+
 # Resume_Builder
 
 Creates a PDF file of a basic resume
--- index.js
+++ index.js
@@ -20,28 +20,31 @@ inquirer
   ])
   .then(function(data) {
     console.log("Favorite color is " + data.color);
-    const queryUrl = `https://api.github.com/users/${data.github}/repos?per_page=100`;
+    // const queryUrl = `https://api.github.com/users/${data.github}/repos?per_page=100`;
+
+    const queryUrl = `https://api.github.com/users/${data.github}`;
     axios.get(queryUrl).then(function(res) {
-      const repoNames = res.data.map(function(repo) {
-        return repo.name;
-      });
-
-      const repoNamesStr = repoNames.join("\n");
-
-      console.log(`Saved ${repoNamesStr} repos`);
+      const prof_img = res.data.avatar_url; //profile image
+      const username = res.data.login; //github username
+      // const location =  res.data.location; User location via Google Maps (link)
+      // const = User GitHub profile (link)
+      const blog = res.data.blog; //link to blog
+      const bio = res.data.bio; //bio from profile
+      const repos = res.data.public_repos; //Number of public repositories
+      const followers = res.data.followers; //Number of followers
+      // const stars = res.data.public_gists; //Number of GitHub stars - this is not right - may have to create array from other git request and get length of it
+      const following = res.data.following; //Number person is following
+      
+      //anvaka should have 54 stars
+      console.log(prof_img);
+      console.log(username);
+      console.log(blog);
+      console.log(bio);
+      console.log(repos);
+      console.log(followers);
+      // console.log(stars);
+      console.log(following);
     });
-    //get this from GitHUb:
-    //       - Profile image
-    // - User name
-    // - Links to the following:
-    //   - User location via Google Maps
-    //   - User GitHub profile
-    //   - User blog
-    // - User bio
-    // - Number of public repositories
-    // - Number of followers
-    // - Number of GitHub stars
-    // - Number of users following
   });
 // }
 
